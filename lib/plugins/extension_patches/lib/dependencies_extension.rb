898223117fbe2c849a2cf09441c27f9c14541cd9
Edmundo Valle Neto <edmundo.vn@gmail.com>
2009-01-11 23:09:26 -0200

    Excluded all the logger lines created for debug purposes from the app override code.

--- lib/plugins/extension_patches/lib/dependencies_extension.rb
+++ lib/plugins/extension_patches/lib/dependencies_extension.rb
@@ -27,21 +27,15 @@ module Spree::DependenciesExtension
     if file_type != 'unknown'
       # First load code from Spree.
       spree_file_name = File.join(SPREE_ROOT, 'app', "#{file_type}s", base_name)
-      RAILS_DEFAULT_LOGGER.info "Checking Spree '#{SPREE_ROOT}' for '#{base_name}'"
       if File.file?("#{spree_file_name}.rb")
-        RAILS_DEFAULT_LOGGER.info "==> Loading from Spree '#{base_name}'"
         file_loaded = true if require_or_load_without_extensions_additions(spree_file_name, const_path)
-      else
-        RAILS_DEFAULT_LOGGER.info "==> Not found in Spree '#{base_name}'"
       end
 
       # Then load code from extensions in the order they were loaded.
       paths_to_extensions = Spree::ExtensionLoader.instance.load_extension_roots
       paths_to_extensions.each do |extension_path|
         extension_file_name = File.expand_path(File.join(extension_path, 'app', "#{file_type}s", base_name))
-        RAILS_DEFAULT_LOGGER.info "Checking extension '#{extension_path}' for '#{base_name}'"
         if File.file?("#{extension_file_name}.rb")
-          RAILS_DEFAULT_LOGGER.info "==> Loading from extension '#{extension_path}'"
           file_loaded = true if require_or_load_without_extensions_additions(extension_file_name, const_path)
         end
       end
